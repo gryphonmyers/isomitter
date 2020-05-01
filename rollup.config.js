a1634ee563af6efba8e508e637d5da0a476605f2
Gryphon Myers <gryphon@homunkul.us>
2020-04-30 23:03:35 -0700

    set up rollup and bundlesize

--- rollup.config.js
+++ rollup.config.js
@@ -1,52 +1,52 @@
 import { terser } from "rollup-plugin-terser";
 
 export default [{
-    input: './lib/isomitter-node.js',
+    input: './lib/node.js',
     output: {
       file: 'dist/isomitter-node.cjs.js',
       format: 'cjs'
     }
 }, {
-    input: './lib/isomitter-node.js',
+    input: './lib/node.js',
     output: {
       file: 'dist/isomitter-node.cjs.min.js',
       plugins: [terser()],
       format: 'cjs'
     }
 }, {
-    input: './lib/isomitter-node.js',
+    input: './lib/node.js',
     output: {
       file: 'dist/isomitter-node.esm.js',
       format: 'esm'
     }
 }, {
-    input: './lib/isomitter-node.js',
+    input: './lib/node.js',
     output: {
       file: 'dist/isomitter-node.esm.min.js',
       plugins: [terser()],
       format: 'esm'
     }
 },{
-  input: './lib/isomitter-browser.js',
+  input: './lib/browser.js',
   output: {
     file: 'dist/isomitter-browser.cjs.js',
     format: 'cjs'
   }
 }, {
-  input: './lib/isomitter-browser.js',
+  input: './lib/browser.js',
   output: {
     file: 'dist/isomitter-browser.cjs.min.js',
     plugins: [terser()],
     format: 'cjs'
   }
 }, {
-  input: './lib/isomitter-browser.js',
+  input: './lib/browser.js',
   output: {
     file: 'dist/isomitter-browser.esm.js',
     format: 'esm'
   }
 }, {
-  input: './lib/isomitter-browser.js',
+  input: './lib/browser.js',
   output: {
     file: 'dist/isomitter-browser.esm.min.js',
     plugins: [terser()],
